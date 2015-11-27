1a0eb940e2189b1cab1a3a94f547d61f5e0bc859
MarionRZG <cadolle.isoc@gmail.com>
2015-11-27 10:52:11 +0100

    Adding some help texts
    Changes made by Marion on 27.11.2015 to add a few more help sentences in the Community page

--- src/pcp/contenttypes/content/community.py
+++ src/pcp/contenttypes/content/community.py
@@ -28,6 +28,8 @@ CommunitySchema = folder.ATFolderSchema.copy() + atapi.Schema((
                          allowed_types=('Person',),
                          widget=ReferenceBrowserWidget(label='Representative',
                                                        allow_browse=1,
+                                                       description='Main person '\
+                                                       representing the Community. '\,
                                                        startup_directory='/people',
                                                        ),
                         ),
@@ -50,6 +52,8 @@ CommunitySchema = folder.ATFolderSchema.copy() + atapi.Schema((
                        relationship='done_for',
                        multiValued=True,
                        widget=BackReferenceWidget(label='Projects involved',
+                                                  description='List of projects '\
+                                                  involved in this Community. '\,
                                                   visible={'edit':'invisible'},
                                                   ),
                        ),
