5e7058f642ce088522012057f1da4bf8424a3ef3
Raphael Ritz <raphael.ritz@gmail.com>
2016-02-19 15:22:09 +0100

    Start exploring how to supporess the CSRF protection warning for the front page. We will need a more functional frontpage anyway.

---
+++ src/pcp/contenttypes/browser/home_page.py
@@ -0,0 +1,22 @@
+from zope.interface import alsoProvides
+from plone import api
+from plone.protect.interfaces import IDisableCSRFProtection
+from Products.Five.browser import BrowserView
+
+class HomePage(BrowserView):
+    """Logic needed for the homepage view"""
+
+    def __init__(self, context, request):
+        """ Initialize context and request as view multi adaption parameters.
+        """
+        alsoProvides(request, IDisableCSRFProtection)
+        self.context = context
+        self.request = request
+
+
+    
+    def newbie(self):
+        """True if 'Member' role not assigned"""
+        roles = api.user.get_roles(obj=self.context)
+        # print roles
+        return 'Member' not in roles
