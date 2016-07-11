d2b37c8e76b4c4eb8cc50fde1e6cd41bd467d64b
Raphael Ritz <raphael.ritz@gmail.com>
2016-07-11 14:58:04 +0200

    More detailed schemata for service offers and service component offers

--- src/pcp/contenttypes/content/servicecomponentoffer.py
+++ src/pcp/contenttypes/content/servicecomponentoffer.py
@@ -7,16 +7,34 @@ from Products.Archetypes import atapi
 from Products.ATContentTypes.content import folder
 from Products.ATContentTypes.content import schemata
 
-# -*- Message Factory Imported Here -*-
+from archetypes.referencebrowserwidget.widget import ReferenceBrowserWidget
 
 from pcp.contenttypes.interfaces import IServiceComponentOffer
 from pcp.contenttypes.config import PROJECTNAME
+from pcp.contenttypes.content.common import CommonFields
+from pcp.contenttypes.content.common import CommonUtilities
 
-ServiceComponentOfferSchema = folder.ATFolderSchema.copy() + atapi.Schema((
-
-    # -*- Your Archetypes field definitions here ... -*-
-
-))
+ServiceComponentOfferSchema = folder.ATFolderSchema.copy() + atapi.Schema((  
+    atapi.ReferenceField('service_component',
+                         relationship='service_component_offered',
+                         allowed_types=('ServiceComponent',),
+                         widget=ReferenceBrowserWidget(label='Service component offered',
+                                                       description='Reference to the catalog entry of the service component being offered.',
+                                                       allow_browse=1,
+                                                       startup_directory='/catalog',
+                                                      ),
+                     ),
+    atapi.ReferenceField('implementations',
+                         relationship='service_component_implementations_offered',
+                         allowed_types=('ServiceComponentImplementation',),
+                         multiValued=True,
+                         widget=ReferenceBrowserWidget(label='Implementations offered',
+                                                       description='Reference to the catalog entry of the implementations(s) of the service component being offered.',
+                                                       allow_browse=1,
+                                                       startup_directory='/catalog',
+                                                      ),
+                   ),
+)) + CommonFields.copy()
 
 
 schemata.finalizeATCTSchema(
@@ -26,7 +44,7 @@ schemata.finalizeATCTSchema(
 )
 
 
-class ServiceComponentOffer(folder.ATFolder):
+class ServiceComponentOffer(folder.ATFolder, CommonUtilities):
     """Provider offers service components"""
     implements(IServiceComponentOffer)
 
--- src/pcp/contenttypes/content/serviceoffer.py
+++ src/pcp/contenttypes/content/serviceoffer.py
@@ -7,22 +7,64 @@ from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
 
-# -*- Message Factory Imported Here -*-
+from archetypes.referencebrowserwidget.widget import ReferenceBrowserWidget
 
 from pcp.contenttypes.interfaces import IServiceOffer
 from pcp.contenttypes.config import PROJECTNAME
+from pcp.contenttypes.content.common import CommonFields
+from pcp.contenttypes.content.common import CommonUtilities
 
 ServiceOfferSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
+    atapi.ReferenceField('service',
+                         relationship='service_offered',
+                         allowed_types=('Service',),
+                         widget=ReferenceBrowserWidget(label='Service offered',
+                                                       description='Reference to the catalog entry of the service being offered.',
+                                                       allow_browse=1,
+                                                       startup_directory='/catalog',
+                                                      ),
+                     ),
+    atapi.ReferenceField('slas',
+                         relationship='slas_offered',
+                         allowed_types=('Document',),
+                         widget=ReferenceBrowserWidget(label='SLAs offered',
+                                                       description='Potential Service Level Agreements under which the service is being offered.',
+                                                       allow_browse=1,
+                                                       startup_directory='/services',
+                                                      ),
 
-    # -*- Your Archetypes field definitions here ... -*-
-
-))
+                     ),
+    atapi.ReferenceField('contact',
+                         relationship='contact_for',
+                         allowed_types=('Person',),
+                         widget=ReferenceBrowserWidget(label='Contact',
+                                                       description='Contact responsible for this service offer.',
+                                                       allow_browse=1,
+                                                       startup_directory='/people',
+                                                      ),
+                     ),
+    atapi.TextField('constraints',
+                    required=False,
+                    searchable=True,
+                    storage=atapi.AnnotationStorage(migrate=True),
+                    validators=('isTidyHtmlWithCleanup',),
+                    default_output_type='text/x-html-safe',
+                    widget=atapi.RichWidget(
+                        description=u'Any constraints under which this service '\
+                        'can only be made available should be described here. '\
+                        'If there are none leave this field empty. Other comments '\
+                        'should go into the "text" field below.',
+                        label=u'Constraints',
+                        rows=15,
+                        ),
+                ),
+)) + CommonFields.copy()
 
 
 schemata.finalizeATCTSchema(ServiceOfferSchema, moveDiscussion=False)
 
 
-class ServiceOffer(base.ATCTContent):
+class ServiceOffer(base.ATCTContent, CommonUtilities):
     """Providers signal their service offerings"""
     implements(IServiceOffer)
 
