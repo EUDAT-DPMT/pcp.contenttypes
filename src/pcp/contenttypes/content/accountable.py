8b33b24bee44eee767dbd2e3d205f7d59837d953
Andreas Jung <info@zopyx.com>
2017-01-17 08:13:13 +0100

    minor cleanup

--- src/pcp/contenttypes/content/accountable.py
+++ src/pcp/contenttypes/content/accountable.py
@@ -17,17 +17,14 @@ class Accountable(object):
     def addAccount(self):
         """ Create IAccount folder on the accounting server """
 
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(ISettings)
-        f = furl.furl(settings.accounting_url + '/addAccount')
-
         owner_id = self.getProvider().provider_userid
         if not owner_id:
             raise ValueError('Provider object has no provider_userid set')
 
-        data = dict(
-            id=self.UID(),
-            owner=owner_id)
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ISettings)
+        f = furl.furl(settings.accounting_url + '/addAccount')
+        data = dict(id=self.UID(), owner=owner_id)
         result = requests.post(f, data=data)
         if not result.ok:
             raise RuntimeError('Unable to create account for {} on accounting server (reason: {})'.format(owner_id, result.text))
