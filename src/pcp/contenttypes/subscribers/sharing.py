5d7d284286bc554764b79a721da7214a14aaff12
Andreas JungÂ <info@zopyx.com>
2017-01-18 07:32:00 +0100

    preliminary interception of 'settings' and 'inherit' from plone.app.workflow

--- src/pcp/contenttypes/subscribers/sharing.py
+++ src/pcp/contenttypes/subscribers/sharing.py
@@ -1,14 +1,22 @@
 
-
+import inspect
 from zopyx.plone.persistentlogger.logger import IPersistentLogger
 
 def SharingHandler(event):
-    context = event.object
-    request = event.descriptions[0]
-    form = request.form
+    """ Log/intercept @@sharing changes """
 
+    context = event.object
     logger = IPersistentLogger(context)
-    print context
-    logger.log('Sharing updated',
-            level='info',
-            details=dict(request.form))
+
+    # *very evil* search the call stack for local variable 'settings' and 'inherit' in order
+    # to find the caller issuing the notify() call.
+    # plone.app.workflow-2.1.9-py2.7.egg/plone/app/workflow/browser/sharing.py
+
+    for frame in inspect.stack():
+        if 'settings' in frame[0].f_locals:
+            settings = frame[0].f_locals['settings']
+            inherit = frame[0].f_locals['inherit']
+            logger.log('Sharing updated',
+                    level='info',
+                    details=dict(inherit=inherit, settings=settings))
+            continue
