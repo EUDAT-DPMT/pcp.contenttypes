13c971c85a5ef3433bb3b79882a063c6bddff22d
Andreas JungÂ <info@zopyx.com>
2017-01-19 09:19:15 +0100

    updated docs

--- src/pcp/contenttypes/interfaces/settings.py
+++ src/pcp/contenttypes/interfaces/settings.py
@@ -7,6 +7,7 @@ class ISettings(Interface):
 
     accounting_url = schema.TextLine(
         title=u'Connection accounting server',
+        description=u'Use http://username:password@host:port/path/to/domain where username+password represent a Manager account on the accounting server',
         default=u'http://accounting.eudat.eu',
         required=True
     )
