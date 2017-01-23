5d7465884dccf195eaf2946818c4fcc9919d319c
Andreas JungÂ <info@zopyx.com>
2017-01-23 13:48:40 +0100

    added monkey patch for handle() of SharingView in order to intercept changes made through the @@sharing view

--- src/pcp/contenttypes/patches.py
+++ src/pcp/contenttypes/patches.py
@@ -38,3 +38,81 @@ patched_field_mapping = lambda: NEW_AT_FIELD_MAPPING
 def apply_patched_mapping(scope, original, replacement):
     setattr(scope, original, replacement())
     return
+
+
+#################################################################################################
+# $HOME/.buildout/eggs/plone.app.workflow-2.1.9-py2.7.egg/plone/app/workflow/browser/sharing.py
+#################################################################################################
+
+from plone.app.workflow.events import LocalrolesModifiedEvent
+from plone.app.workflow import PloneMessageFactory as _
+from Products.statusmessages.interfaces import IStatusMessage
+from zope.event import notify
+
+
+def sharing_handle_form(self):
+    """
+    We split this out so we can reuse this for ajax.
+    Will return a boolean if it was a post or not
+    """
+
+    postback = True
+
+    form = self.request.form
+    submitted = form.get('form.submitted', False)
+    save_button = form.get('form.button.Save', None) is not None
+    cancel_button = form.get('form.button.Cancel', None) is not None
+    if submitted and save_button and not cancel_button:
+        if not self.request.get('REQUEST_METHOD', 'GET') == 'POST':
+            raise Forbidden
+
+        old_ac_local_roles_block = getattr(self.context, '__ac_local_roles_block__', None)
+
+        authenticator = self.context.restrictedTraverse('@@authenticator',
+                                                        None)
+        if not authenticator.verify():
+            raise Forbidden
+
+        # Update the acquire-roles setting
+        if self.can_edit_inherit():
+            inherit = bool(form.get('inherit', False))
+            reindex = self.update_inherit(inherit, reindex=False)
+        else:
+            reindex = False
+
+        # Update settings for users and groups
+        entries = form.get('entries', [])
+        roles = [r['id'] for r in self.roles()]
+        settings = []
+        for entry in entries:
+            settings.append(
+                dict(id=entry['id'],
+                     type=entry['type'],
+                     roles=[r for r in roles
+                        if entry.get('role_%s' % r, False)]))
+        if settings:
+            reindex = self.update_role_settings(settings, reindex=False) \
+                        or reindex
+        if reindex:
+            self.context.reindexObjectSecurity()
+            notify(LocalrolesModifiedEvent(self.context, self.request))
+
+
+        new_ac_local_roles_block = getattr(self.context, '__ac_local_roles_block__', None)
+
+        print old_ac_local_roles_block
+        print new_ac_local_roles_block
+
+
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"Changes saved."), type='info')
+
+    # Other buttons return to the sharing page
+    if cancel_button:
+        postback = False
+
+    return postback
+
+from plone.app.workflow.browser.sharing import SharingView
+SharingView.handle_form = sharing_handle_form
+
