f60e7da24f7634c1587496d12533e8e91edb6fa3
Andreas JungÂ <info@zopyx.com>
2017-01-24 14:54:14 +0100

    logging new diff_context

--- src/pcp/contenttypes/subscribers/sharing.py
+++ src/pcp/contenttypes/subscribers/sharing.py
@@ -7,16 +7,8 @@ def SharingHandler(event):
 
     context = event.object
     logger = IPersistentLogger(context)
+    print event.diff_context
+    logger.log('Sharing updated',
+            level='info',
+            details=event.diff_context)
 
-    # *very evil* search the call stack for local variable 'settings' and 'inherit' in order
-    # to find the caller issuing the notify() call.
-    # plone.app.workflow-2.1.9-py2.7.egg/plone/app/workflow/browser/sharing.py
-
-    for frame in inspect.stack():
-        if 'settings' in frame[0].f_locals:
-            settings = frame[0].f_locals['settings']
-            inherit = frame[0].f_locals['inherit']
-            logger.log('Sharing updated',
-                    level='info',
-                    details=dict(inherit=inherit, settings=settings))
-            continue
