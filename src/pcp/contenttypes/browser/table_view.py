fe95269a8b4d54079e1cd4e8e834d1463d2def6c
Andreas JungÂ <info@zopyx.com>
2017-01-25 15:13:28 +0100

    added table-view

--- src/pcp/contenttypes/browser/table_view.py
+++ src/pcp/contenttypes/browser/table_view.py
@@ -2,18 +2,39 @@
 from Products.Five.browser import BrowserView
 
 
+hidden_fields = (
+    'id',
+    'title',
+    'description',
+    'constrainTypesMode',
+    'locallyAllowedTypes',
+    'immediatelyAddableTypes',
+    'location',
+    'language',
+    'effectiveDate',
+    'creation_date',
+    'modification_date',
+    'creators',
+    'excludeFromNav',
+    'nextPreviousEnabled'
+)
+
+hidden_fields = dict([(f, 0) for f in hidden_fields])
+
+
 class TableView(BrowserView):
     """ Custom view (replaces base_view of Archetypes """
 
     def data(self):
-
         result = list()
         for field in self.context.Schema().fields():
+            if field.getName() in hidden_fields:
+                continue
             value = field.get(self.context)
             if value in (None, '', [], ()):
                 continue
             result.append(dict(
-                title=field.getName(),
+                field=field.getName(),
+                title=field.widget.Label(self.context),
                 value=value))
         return result
-
