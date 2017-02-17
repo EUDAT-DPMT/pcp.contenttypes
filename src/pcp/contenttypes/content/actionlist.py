b5cb854b1f46f87b43f3f4b5799c47fdd644c668
Andreas JungÂ <info@zopyx.com>
2017-02-17 10:57:09 +0100

    added Ref field for action items

--- src/pcp/contenttypes/content/actionlist.py
+++ src/pcp/contenttypes/content/actionlist.py
@@ -6,8 +6,10 @@ from zope.interface import implements
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import schemata
 from Products.ATContentTypes.content.schemata import ATContentTypeSchema
-from Products.ATContentTypes.content.schemata import relatedItemsField
-# -*- Message Factory Imported Here -*-
+from Products.Archetypes.atapi import ReferenceField
+from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import ReferenceBrowserWidget
+from Products.CMFCore.permissions import ModifyPortalContent
+from Products.ATContentTypes import ATCTMessageFactory as _
 
 from pcp.contenttypes.interfaces import IActionList
 from pcp.contenttypes.config import PROJECTNAME
@@ -15,7 +17,25 @@ from pcp.contenttypes.config import PROJECTNAME
 
 ActionListSchema = ATContentTypeSchema.copy() + atapi.Schema((
 
-    # -*- Your Archetypes field definitions here ... -*-
+    ReferenceField('actionItems',
+            relationship = 'hasActionItem',
+            multiValued = True,
+            isMetadata = True,
+            languageIndependent = False,
+            index = 'KeywordIndex',
+            write_permission = ModifyPortalContent,
+            widget = ReferenceBrowserWidget(
+                allow_search = True,
+                allow_browse = True,
+                show_indexes = False,
+                allow_sorting=1,
+                force_close_on_insert = True,
+                base_query={'portal_type': 'ActionItem'},
+                label = _(u'label_action_items', default=u'Action items'),
+                description = '',
+                visible = {'edit' : 'visible', 'view' : 'invisible' }
+                )
+            )
 
 ))
 
