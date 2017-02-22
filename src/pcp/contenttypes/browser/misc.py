aed58624a2292a0fbbbd28b8338fb743a2197982
Andreas JungÂ <info@zopyx.com>
2017-02-22 13:15:44 +0100

    updated

--- src/pcp/contenttypes/browser/misc.py
+++ src/pcp/contenttypes/browser/misc.py
@@ -23,7 +23,16 @@ class Misc(BrowserView):
             form = brain.getObject()
             has_service = 'service' in form.objectIds()
             has_save_adapter = len(form.objectValues('FormSaveData2ContentAdapter')) > 0
+            save_adapter_url = None
+            saved_forms = 0
+            if has_save_adapter:
+                save_adapter = form.objectValues('FormSaveData2ContentAdapter')[0]
+                saved_forms = len(save_adapter.objectIds())
+                save_adapter_url = save_adapter.absolute_url() + '/folder_contents'
+
             result.append(dict(
+                saved_forms=saved_forms,
+                save_adapter_url=save_adapter_url,
                 has_service=has_service,
                 has_save_adapter=has_save_adapter,
                 url=brain.getURL(),
