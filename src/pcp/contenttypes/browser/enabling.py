a3794e6a59704e7cf5729b58f92409609c082d76
Andreas JungÂ <info@zopyx.com>
2017-02-23 09:55:58 +0100

    added missing link between action lists, form and poi tracker generation

--- src/pcp/contenttypes/browser/enabling.py
+++ src/pcp/contenttypes/browser/enabling.py
@@ -9,7 +9,18 @@ class Enabling(BrowserView):
     def getServices(self):
         """ For PFG enabling forms ('service' field) """
         catalog = plone.api.portal.get_tool('portal_catalog')
-        query = dict(portal_type='Service', sort_on='sortable_title')
+        path = '{}/catalog'.format('/'.join(plone.api.portal.get().getPhysicalPath()))
+        query = dict(portal_type='Service', sort_on='sortable_title', path=path)
+        result = list()
+        for brain in catalog(**query):
+            result.append((brain.getId, brain.Title))
+        return result
+
+    def getProjects(self):
+        """ For PFG enabling forms ('project' field) """
+        catalog = plone.api.portal.get_tool('portal_catalog')
+        path = '{}/projects'.format('/'.join(plone.api.portal.get().getPhysicalPath()))
+        query = dict(portal_type='Project', sort_on='sortable_title', path=path)
         result = list()
         for brain in catalog(**query):
             result.append((brain.getId, brain.Title))
@@ -23,6 +34,7 @@ class Enabling(BrowserView):
         for brain in catalog(**query):
             form = brain.getObject()
             has_service = 'service' in form.objectIds()
+            has_project = 'project' in form.objectIds()
             has_save_adapter = len(form.objectValues('FormSaveData2ContentAdapter')) > 0
             save_adapter_url = None
             saved_forms = 0
@@ -34,7 +46,53 @@ class Enabling(BrowserView):
                 saved_forms=saved_forms,
                 save_adapter_url=save_adapter_url,
                 has_service=has_service,
+                has_project=has_project,
                 has_save_adapter=has_save_adapter,
                 url=brain.getURL(),
                 title=brain.Title))
         return result
+
+    def process_enabling_form_data(self):
+
+        catalog = plone.api.portal.get_tool('portal_catalog')
+
+        # find associated service
+        service_name = self.context.Schema().getField('service').get(self.context)
+        path = '{}/catalog'.format('/'.join(plone.api.portal.get().getPhysicalPath()))
+        brains = catalog(dict(portal_type='Service', getId=service_name, path=path))
+        if not brains:
+            raise ValueError('Service {} not found'.format(service_name))
+        service = brains[0].getObject()
+
+        # find associated service
+        project_name = self.context.Schema().getField('project').get(self.context)
+        path = '{}/projects'.format('/'.join(plone.api.portal.get().getPhysicalPath()))
+        brains = catalog(dict(portal_type='Project', getId=project_name, path=path))
+        if not brains:
+            raise ValueError('Project {} not found'.format(project_name))
+        project = brains[0].getObject()
+
+        # find ActionLists associated with the service
+        brains = catalog(dict(portal_type='ActionList'))
+        action_lists = [brain.getObject() for brain in brains]
+        action_lists = [al for al in action_lists if al.getService() == service]
+
+        filter_method = 'filter_{}'.format(service_name)
+        method = getattr(self, filter_method, None)
+        if not method:
+            raise ValueError('No filtering method {}() implemented'.format(filter_method))
+
+
+        action_lists = method(project, service, action_lists)
+
+        for al in action_lists:
+            al.createPOI(target_path='/'.join(project.getPhysicalPath()))
+
+        return 'DONE'
+
+
+    def filter_B2SAFE(self, project, service, action_lists):
+        """ Do some filtering...."""
+
+        return action_lists
+
