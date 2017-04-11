f8c7bf3c303e2a2a87419b09ae82dc165e8ee1d9
Bernhard Pöttinger <bernhard.poettinger@tngtech.com>
2017-04-11 10:48:50 +0200

    Make downtime test pass with new datetime widget

--- src/pcp/contenttypes/tests/test_downtime.py
+++ src/pcp/contenttypes/tests/test_downtime.py
@@ -25,26 +25,24 @@ class TestDowntime(FunctionalTestCase):
     TEST_PROVIDER_COUNTRY = 'Afghanistan'
     TEST_DOWNTIME_TITLE = 'Das ganze Scheisshaus steht in Flammen'
     TEST_DOWNTIME_DESCRIPTION = 'Sogar der Arsch ist in Gefahr'
-    TEST_DOWNTIME_START = ('2017', 'July', '13', '05', '15', 'AM')
-    TEST_DOWNTIME_START_STRING = '2017-07-13 05:15 UTC'
-    TEST_DOWNTIME_END = ('2017', 'July', '14', '06', '30', 'PM')
-    TEST_DOWNTIME_END_STRING = '2017-07-14 18:30 UTC'
+    TEST_DOWNTIME_START = ('2017', 'July', '13', '05', '15')
+    TEST_DOWNTIME_START_STRING = '2017/07/13 05:15:00 UTC'
+    TEST_DOWNTIME_END = ('2017', 'July', '14', '18', '30')
+    TEST_DOWNTIME_END_STRING = '2017/07/14 18:30:00 UTC'
 
     def fillInDateWidget(self, browser, field, date):
-        browser.getControl(name=field+'_year').displayValue = [date[0]]
-        browser.getControl(name=field+'_month').displayValue = [date[1]]
-        browser.getControl(name=field+'_day').displayValue = [date[2]]
-        browser.getControl(name=field+'_hour').displayValue = [date[3]]
-        browser.getControl(name=field+'_minute').displayValue = [date[4]]
-        browser.getControl(name=field+'_ampm').displayValue = [date[5]]
+        browser.getControl(name=field+'-year').displayValue = [date[0]]
+        browser.getControl(name=field+'-month').displayValue = [date[1]]
+        browser.getControl(name=field+'-day').displayValue = [date[2]]
+        browser.getControl(name=field+'-hour').displayValue = [date[3]]
+        browser.getControl(name=field+'-minute').displayValue = [date[4]]
 
     def assertDateWidget(self, browser, field, date):
-        self.assertEquals(browser.getControl(name=field+'_year').displayValue[0], date[0])
-        self.assertEquals(browser.getControl(name=field+'_month').displayValue[0], date[1])
-        self.assertEquals(browser.getControl(name=field+'_day').displayValue[0], date[2])
-        self.assertEquals(browser.getControl(name=field+'_hour').displayValue[0], date[3])
-        self.assertEquals(browser.getControl(name=field+'_minute').displayValue[0], date[4])
-        self.assertEquals(browser.getControl(name=field+'_ampm').displayValue[0], date[5])
+        self.assertEquals(browser.getControl(name=field+'-year').displayValue[0], date[0])
+        self.assertEquals(browser.getControl(name=field+'-month').displayValue[0], date[1])
+        self.assertEquals(browser.getControl(name=field+'-day').displayValue[0], date[2])
+        self.assertEquals(browser.getControl(name=field+'-hour').displayValue[0], date[3])
+        self.assertEquals(browser.getControl(name=field+'-minute').displayValue[0], date[4])
 
     def test(self):
         # Create a provider and within the provider a downtime for it.
@@ -114,8 +112,8 @@ class TestDowntime(FunctionalTestCase):
         # TODO: Then check for existence of downtime in portlet (provider specific portlet view and common view)
 
         self.assertTrue(self.TEST_DOWNTIME_TITLE in browser.contents)
-        self.assertTrue(self.TEST_DOWNTIME_END_STRING in browser.contents)
         self.assertTrue(self.TEST_DOWNTIME_START_STRING in browser.contents)
+        self.assertTrue(self.TEST_DOWNTIME_END_STRING in browser.contents)
         self.assertTrue(self.TEST_DOWNTIME_DESCRIPTION in browser.contents)
 
         browser.open(portal_url + '/downtime_overview')
