4fee17c56f34e94758352ebd4efa75986b9a4a5c
Bernhard Pöttinger <bernhard.poettinger@tngtech.com>
2017-05-23 18:16:18 +0200

    Add subscriber notifying the admin about new users

---
+++ src/pcp/contenttypes/subscribers/principal_created.py
@@ -0,0 +1,23 @@
+import plone
+from pcp.contenttypes.mail import send_mail
+
+
+def principal_created(event):
+    portal = plone.api.portal.get()
+
+    principal = event.principal
+    user = plone.api.portal.get_tool('portal_membership').getMemberById(principal.getId())
+
+    params = {
+        'fullname': user.getProperty('fullname'),
+        'email': user.getProperty('email'),
+        'id': principal.getId()
+    }
+
+    send_mail(
+        sender=None,
+        recipients=[portal.email_from_address],
+        subject='A new user visited DPMT',
+        template='user-created-by-auto-user-maker.txt',
+        params=params,
+        context=None)
