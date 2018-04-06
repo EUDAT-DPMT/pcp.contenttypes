0189161e685a4dfae75fd8ef9f89a30fa62d043e
Raphael Ritz <raphael.ritz@gmail.com>
2018-04-06 15:23:06 +0200

    There is now a minimal star record for registered storage resources

--- src/pcp/contenttypes/browser/star_view.py
+++ src/pcp/contenttypes/browser/star_view.py
@@ -2,30 +2,50 @@
 # Registered Storage Components
 # For specification see: http://cds.cern.ch/record/1452920/files/GFD.201.pdf
 
-from Products.Five.browser import BrowserView
+import time
+from datetime import datetime, timedelta
+
+from pcp.contenttypes.browser.accounting import Accounting
 
 minimal_star_template = """
 <sr:StorageUsageRecord
  xmlns:sr="http://eu-emi.eu/namespaces/2011/02/storagerecord">
-  <sr:RecordIdentity sr:createTime="2010-11-09T09:06:52Z"
-                     sr:recordId="host.example.org/sr/87912469269276"/>
-  <sr:StorageSystem>host.example.org</sr:StorageSystem>
-  <sr:StartTime>2010-10-11T09:31:40Z</sr:StartTime>
-  <sr:EndTime>2010-10-12T09:29:42Z</sr:EndTime>
-  <sr:ResourceCapacityUsed>13617</sr:ResourceCapacityUsed>
+  <sr:RecordIdentity sr:createTime="{endtime}Z"
+                     sr:recordId="accounting.eudat.eu/eudat/{rsr_id}/{record_id}"/>
+  <sr:StorageSystem>{url}</sr:StorageSystem>
+  <sr:StartTime>{starttime}Z</sr:StartTime>
+  <sr:EndTime>{endtime}Z</sr:EndTime>
+  <sr:ResourceCapacityUsed>{usage}</sr:ResourceCapacityUsed>
 </sr:StorageUsageRecord>
 """
 
-class StarView(BrowserView):
+class StarView(Accounting):
     """Render a StAR view for registered storage components"""
-
+    
     def collect_data(self):
         """Helper to collect the values to be rendered as XML"""
         context = self.context
+        records = self.records()
+        if records:
+            latest = records[0]
+        else:
+            latest = {'core': {'value': 0, 'unit': 'byte'}, 
+                      'meta': {ts: time.time()}}
+
         result = {}
         result['id'] = context.getId()
         result['title'] = context.Title()
         result['description'] = context.Description()
+        result['url'] = context.absolute_url()
+        result['rsr_id'] = context.UID()
+        result['record_id'] = latest['meta'].get('ts')
+        ts = latest['meta'].get('ts')
+        endtime = datetime.utcfromtimestamp(ts)
+        result['endtime'] = endtime.isoformat()
+        result['starttime'] = (endtime - timedelta(days=1)).isoformat()
+        # this should be in byte without unit
+        result['usage'] = ' '.join([latest['core'].get('value'), 
+                                    latest['core'].get('unit')])
         return result
 
     def star(self):
