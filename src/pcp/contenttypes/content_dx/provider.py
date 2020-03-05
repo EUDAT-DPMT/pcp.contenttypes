9852de16ca1b6e7df6a2710e5e7c915b14cdbcba
Philip Bauer <bauer@starzel.de>
2020-03-05 15:25:45 +0100

    add relationlist and configure relationchoice

--- src/pcp/contenttypes/content_dx/provider.py
+++ src/pcp/contenttypes/content_dx/provider.py
@@ -1,10 +1,13 @@
 # -*- coding: UTF-8 -*-
 from collective import dexteritytextindexer
+from plone.app.multilingual.browser.interfaces import make_relation_root_path
 from plone.app.vocabularies.catalog import CatalogSource
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.autoform import directives
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
 from zope import schema
 from zope.interface import implementer
 
@@ -27,27 +30,27 @@ class IProvider(model.Schema):
         "supported_os",
     )
 
-    url = schema.URI(title=u"Url", required=False,)
+    url = schema.URI(title=u"Url", required=False)
 
     # Computed field 'link2offers'
 
-    provider_type = schema.TextLine(title=u"Provider type", required=False,)
+    provider_type = schema.TextLine(title=u"Provider type", required=False)
 
-    provider_userid = schema.TextLine(title=u"Provider user ID", required=True,)
+    provider_userid = schema.TextLine(title=u"Provider user ID", required=True)
 
-    provider_status = schema.TextLine(title=u"Provider status", required=False,)
+    provider_status = schema.TextLine(title=u"Provider status", required=False)
 
-    status_details = schema.TextLine(title=u"Status details/comment", required=False,)
+    status_details = schema.TextLine(title=u"Status details/comment", required=False)
 
-    infrastructure = schema.TextLine(title=u"Infrastructure", required=False,)
+    infrastructure = schema.TextLine(title=u"Infrastructure", required=False)
 
-    domain = schema.TextLine(title=u"Domain", required=False,)
+    domain = schema.TextLine(title=u"Domain", required=False)
 
     # Address field
 
-    VAT = schema.TextLine(title=u"VAT", required=False,)
+    VAT = schema.TextLine(title=u"VAT", required=False)
 
-    timezone = schema.TextLine(title=u"Time zone", required=False,)
+    timezone = schema.TextLine(title=u"Time zone", required=False)
 
     latitude = schema.TextLine(
         title=u"Latitude",
@@ -62,43 +65,78 @@ class IProvider(model.Schema):
     )
 
     ip4range = schema.TextLine(
-        title=u"IPv4 range", description=u"(a.b.c.d/e.f.g.h)", required=False,
+        title=u"IPv4 range", description=u"(a.b.c.d/e.f.g.h)", required=False
     )
 
-    ip6range = schema.TextLine(title=u"IPv6 range", required=False,)
+    ip6range = schema.TextLine(title=u"IPv6 range", required=False)
 
     contact = RelationChoice(
         title=u"Contact",
         description=u"Main contact person for the operations of this provider.",
-        source=CatalogSource(portal_type=["Person"]),
+        source=CatalogSource(),
         required=False,
+    )
+    directives.widget(
+        "contact",
+        RelatedItemsFieldWidget,
+        pattern_options={
+            "selectableTypes": ["person_dx"],
+            "basePath": make_relation_root_path,
+        },
     )
 
     business_contact = RelationChoice(
         title=u"Business contact",
         description=u"Main contact person for the financial matters of this provider.",
-        source=CatalogSource(portal_type=["Person"]),
+        source=CatalogSource(),
         required=False,
+    )
+    directives.widget(
+        "business_contact",
+        RelatedItemsFieldWidget,
+        pattern_options={
+            "selectableTypes": ["person_dx"],
+            "basePath": make_relation_root_path,
+        },
     )
 
     security_contact = RelationChoice(
         title=u"Security contact",
         description=u"Person specifically to be contacted for security-related matters.",
-        source=CatalogSource(portal_type=["Person"]),
+        source=CatalogSource(),
         required=False,
     )
+    directives.widget(
+        "security_contact",
+        RelatedItemsFieldWidget,
+        pattern_options={
+            "selectableTypes": ["person_dx"],
+            "basePath": make_relation_root_path,
+        },
+    )
 
-    # to be implemented later
-    # admins = RelationList(
-    #         title=u'Administrators',
-    #         source=CatalogSource(portal_type=['Person']),
-    #         )
+    admins = RelationList(
+        title=u"Administrators",
+        default=[],
+        value_type=RelationChoice(source=CatalogSource()),
+        required=False,
+        missing_value=[],
+    )
+    directives.widget(
+        "admins",
+        RelatedItemsFieldWidget,
+        pattern_options={
+            "selectableTypes": ["person_dx"],
+            "basePath": make_relation_root_path,
+        },
+    )
 
     emergency_phone = schema.TextLine(
         title=u"Emergency telephone number",
         description=u"Include international prefix and area code",
         required=False,
     )
+
     # alarm email
     # helpdesk email
     # supported os
