ed655c057096fab9be59d41d4fb6a4ad8c7ecc18
Philip Bauer <bauer@starzel.de>
2020-06-05 11:38:21 +0200

    add address to community same as for provider

--- src/pcp/contenttypes/content_dx/community.py
+++ src/pcp/contenttypes/content_dx/community.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 from collective import dexteritytextindexer
 from pcp.contenttypes.backrels.backrelfield import BackrelField
+from pcp.contenttypes.widgets import TrustedTextWidget
 from plone import api
 from plone.app.multilingual.browser.interfaces import make_relation_root_path
 from plone.app.vocabularies.catalog import CatalogSource
@@ -8,6 +9,7 @@ from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.autoform import directives
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from z3c.form.interfaces import IDisplayForm
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope import schema
@@ -22,7 +24,36 @@ class ICommunity(model.Schema):
 
     url = schema.URI(title=u"Url", required=False,)
 
-    # AddressField address
+    # hide adress fields from display. instead show a condensed view from the property 'address'
+    directives.omitted(IDisplayForm, 'street1', 'street2', 'zip', 'city', 'country')
+    street1 = schema.TextLine(
+        title=u'Street 1',
+        required=False,
+    )
+
+    street2 = schema.TextLine(
+        title=u'Street 2',
+        required=False,
+    )
+
+    zip = schema.TextLine(
+        title=u'ZIP code',
+        required=False,
+    )
+
+    city = schema.TextLine(
+        title=u'City',
+        required=True,
+    )
+
+    country = schema.Choice(
+        title=u'Country',
+        vocabulary='dpmt.country_names',
+        required=True,
+    )
+
+    address = schema.TextLine(title=u'Adress', readonly=True)
+    directives.widget('address', TrustedTextWidget)
 
     VAT = schema.TextLine(title=u"VAT", required=False,)
 
@@ -98,3 +129,17 @@ class ICommunity(model.Schema):
 @implementer(ICommunity)
 class Community(Container):
     """Community instance"""
+
+    @property
+    def address(self):
+        street = None
+        if self.street1 and self.street2:
+            street = u'{} {}'.format(self.street1, self.street2)
+        elif self.street1 and not self.street2:
+            street = self.street1
+        elif not self.street1 and self.street2:
+            street = self.street2
+
+        city = u'{} {}'.format(self.zip, self.city) if self.zip else self.city
+        items = [street, city, self.country]
+        return u'<br />'.join(i for i in items if i)
