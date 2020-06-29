9fc77117b787d92bf3e8f7448673f5f1104308be
Philip Bauer <bauer@starzel.de>
2020-06-29 17:19:56 +0200

    add helper to display a content-value using the default widget

---
+++ src/pcp/contenttypes/browser/widget.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+"""
+Example use in a template:
+<div tal:define="widget_view nocall:context/@@widget_view">
+  <div class="field"
+       tal:define="widget python:widget_view.get_widget(obj, 'projects_involved')"
+       tal:condition="python:widget">
+      <label tal:content="widget/label" />
+      <br />
+      <div tal:content="structure widget/render" />
+  </div>
+</div>
+
+For behaviors pass interface and fieldname,
+e.g. get_widget(obj, 'IRichTextBehavior.text')
+"""
+from plone.dexterity.browser.view import DefaultView
+from Products.Five.browser import BrowserView
+
+
+class WidgetView(BrowserView):
+    """Get the widget for any field on a content object.
+    Useful to get the default-rendering as configured in the schema.
+    """
+
+    def get_widget(self, obj, fieldname):
+        default_view = DefaultView(obj, self.request)
+        default_view.update()
+        if fieldname in default_view.w:
+            return default_view.w[fieldname]
