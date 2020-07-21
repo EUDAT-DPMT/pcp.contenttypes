42ef9dc6bd8fb0faad18e51ea96d3f43c7162b0b
Philip Bauer <bauer@starzel.de>
2020-07-21 10:33:14 +0200

    fix reinstall and remove some obsolete AT stuff

--- src/pcp/contenttypes/config.py
+++ src/pcp/contenttypes/config.py
@@ -1,37 +1,3 @@
 """Common configuration constants
 """
-
 PROJECTNAME = 'pcp.contenttypes'
-
-ADD_PERMISSIONS = {
-    # -*- extra stuff goes here -*-
-    'RegisteredStorageResource': 'pcp.contenttypes: Add RegisteredStorageResource',
-    'RegisteredComputeResource': 'pcp.contenttypes: Add RegisteredComputeResource',
-    'ServiceOffer': 'pcp.contenttypes: Add ServiceOffer',
-    'AccountingRecord': 'pcp.contenttypes: Add AccountingRecord',
-    'Downtime': 'pcp.contenttypes: Add Downtime',
-    'ServiceComponentImplementationDetails': 'pcp.contenttypes: Add ServiceComponentImplementationDetails',
-    'ServiceComponentImplementation': 'pcp.contenttypes: Add ServiceComponentImplementation',
-    'ServiceComponent': 'pcp.contenttypes: Add ServiceComponent',
-    'ServiceDetails': 'pcp.contenttypes: Add Service Details',
-    'RegisteredResource': 'pcp.contenttypes: Add RegisteredResource',
-    'RegisteredServiceComponent': 'pcp.contenttypes: Add RegisteredServiceComponent',
-    'RegisteredService': 'pcp.contenttypes: Add RegisteredService',
-    'ResourceRequest': 'pcp.contenttypes: Add ResourceRequest',
-    'ServiceComponentRequest': 'pcp.contenttypes: Add ServiceComponentRequest',
-    'ServiceRequest': 'pcp.contenttypes: Add ServiceRequest',
-    'ServiceComponentOffer': 'pcp.contenttypes: Add ServiceComponentOffer',
-    'ResourceOffer': 'pcp.contenttypes: Add ResourceOffer',
-    'Plan': 'pcp.contenttypes: Add Plan',
-    'Environment': 'pcp.contenttypes: Add Environment',
-    'Provider': 'pcp.contenttypes: Add Provider',
-    'Person': 'pcp.contenttypes: Add Person',
-    'Resource': 'pcp.contenttypes: Add Resource',
-    'Center': 'pcp.contenttypes: Add Center',
-    'Community': 'pcp.contenttypes: Add Community',
-    'Project': 'pcp.contenttypes: Add Project',
-    'Service': 'pcp.contenttypes: Add Service',
-    'RoleRequest': 'pcp.contenttypes: Add RoleRequest',
-    'ActionItem': 'pcp.contenttypes: Add ActionItem',
-    'ActionList': 'pcp.contenttypes: Add ActionIList',
-}
