0d77d76a687b5c6662b44fcd0eaa2ccc9327f04d
Philip Bauer <bauer@starzel.de>
2020-08-27 20:26:18 +0200

    override default dashboard

---
+++ src/pcp/contenttypes/dashboard.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from pcp.contenttypes.portlets import downtimes
+from plone.app.portlets import portlets
+from plone.app.portlets.dashboard import DefaultDashboard
+from plone.app.portlets.interfaces import IDefaultDashboard
+from zope.interface import implementer
+
+
+@implementer(IDefaultDashboard)
+class PCPDefaultDashboard(DefaultDashboard):
+    """A overrride of the default dashboard.
+    """
+
+    def __call__(self):
+        return {
+            'plone.dashboard1': (portlets.news.Assignment(), ),
+            'plone.dashboard2': (portlets.recent.Assignment(), ),
+            'plone.dashboard3': (downtimes.Assignment(), ),
+            'plone.dashboard4': (portlets.review.Assignment(), ),
+        }
